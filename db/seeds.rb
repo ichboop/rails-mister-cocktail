06fba200081dbec7882d00c68997a357b641fcba
Leonard Diemer <leonard.ow.diemer@gmail.com>
2021-02-18 16:53:16 +0100

    t3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+# Ingredient.create_all
+
+# Ingredient.each do |name|
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+  json = open(url).read
+  # json.read
+  #Parse the JSON into a hash
+  parsed = ActiveSupport::JSON.decode(json)
+  # p parsed
+  parsed['drinks'].each do |result|
+    Ingredient.create(name: result['strIngredient1'])
+    # p result
+  end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
